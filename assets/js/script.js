4486d45a7000b608dd910210c55d8daa82ded797
eon <noe.navarro1019@gmail.com>
2021-12-04 12:34:14 -0700

    linked dayjs CDN and passed date object in string form to task schedule header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,12 @@
+// date variables
 var now = dayjs();
-console.log(now);
+
+// pass date time in string format to #currentDay 
+// Day of Week, Month, Day Numbe
+var dateString = now.format('dddd: MMMM D, YYYY');
+
+// html elements
+var currentDayEl = document.getElementById('currentDay');
+
+// set current day string
+currentDayEl.textContent = dateString;
