04c9ce0ce2ed168872299bba0f84931714430828
eon <noe.navarro1019@gmail.com>
2021-12-05 19:07:39 -0700

    updated logic for tasks. They now edit and save as expected. Additionally, they will pull from the local storage if any pre-existing tasks exist

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,11 @@ var now = dayjs();
 var dateString = now.format('dddd: MMMM D, YYYY');
 var date = now.format('YYYY-MM-D');
 
+var test = document.getElementById('text-test');
+
 // import local storage if present
-var taskStorage = localStorage.getItem('taskMemory');
+var taskStorage = JSON.parse(localStorage.getItem('taskMemory'));
+
 // if the current date is not present, establish an empty container
 if (!taskStorage){
     var taskStorage = {
@@ -20,8 +23,17 @@ if (!taskStorage){
                 '4:00 PM': '',
                 '5:00 PM': ''
             }
+        }
+    localStorage.setItem('taskMemory', JSON.stringify(taskStorage));
+} else {
+    // iterate through the local storage and import saved tasks
+    for (task in taskStorage['tasks']){
+        var taskEl = document.getElementById(`${task}`);
+        var descriptionEl = taskEl.nextElementSibling;
+
+        // update descriptionEl with storage value
+        descriptionEl.value = taskStorage['tasks'][task];
     }
-    localStorage.setItem('taskMemory', JSON.stringify(taskStorage))
 }
 
 // html elements
@@ -35,11 +47,22 @@ $('.saveBtn').click(function () {
     // extract task description
     var taskDescription = $(this)
         .siblings('.description')[0]
-        .innerText;
+        .value;
+
     // extract task time slot
     var taskTime = $(this)
         .siblings('.hour')[0]
         .innerText;
 
-
+    // update specific task entry and update local storage
+    taskStorage['tasks'][taskTime] = taskDescription;
+    localStorage.setItem('taskMemory', JSON.stringify(taskStorage));
 })
+
+// click event for task description or creation
+$('.description').click(function () {
+    var text = $(this).value().trim();
+    var newText = $('<textarea>').addClass('description col-sm-8').val(text);
+    $(this).replaceWith(newText);
+})
+
